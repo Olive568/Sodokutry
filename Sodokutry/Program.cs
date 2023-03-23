af88e758046a762c0ce5cd8d11698684979a7f2d
Olive568 <luislabapis@gmail.com>
2023-03-23 17:31:05 +0800

    added sex

--- Sodokutry/Program.cs
+++ Sodokutry/Program.cs
@@ -17,10 +17,14 @@ namespace Sodoku
             List<int>[] rows = new List<int>[9];
             List<int>[] cols = new List<int>[9];
             List<int>[] cells = new List<int>[9];
+            List<int>[] rowsex = new List<int>[9];
+            List<int>[] colsex = new List<int>[9];
+            List<int>[] cellsex = new List<int>[9];
             int[,] board = new int[9, 9];
             Random rnd = new Random();
             bool cont = true;
             int count = 0;
+            bool ohno = false;
 
 
             for (int x = 0; x < 9; x++)
@@ -32,6 +36,9 @@ namespace Sodoku
                 rows[x] = new List<int>();
                 cols[x] = new List<int>();
                 cells[x] = new List<int>();
+                    rowsex[x] = new List<int>();
+                colsex[x] = new List<int>();
+                cellsex[x] = new List<int>();
             }
             for(int x = 0; x < 9; x++)
             {
@@ -40,11 +47,14 @@ namespace Sodoku
                     rows[x].Add(y);
                     cols[x].Add(y);
                     cells[x].Add(y);
+                    rowsex[x].Add(y);
+                    colsex[x].Add(y);
+                    cellsex[x].Add(y);
                 }
             }
             while(cont)
             {
-                
+                ohno = false;
                 cont = true;
                 for(int x = 0; x < 9; x++)
                 {
@@ -54,72 +64,97 @@ namespace Sodoku
                     {
                         
                         temp2 = rows[y];
-                        
-                        if (x <= 2 && y <= 2)
-                        {
-                            g = 0;
-                            temp3 = cells[g];
-                        }
-                        else if (x <= 2 && y <=5)
-                        {
-                            g = 1;
-                            temp3 = cells[g];
-                        }
-                        else if (x <= 2 && y <= 8)
-                        {
-                            g = 2;
-                            temp3 = cells[g];
-                        }
-                        else if (x <= 5 && y <=2)
-                        {
-                            g = 3;
-                            temp3 = cells[g];
-                        }
-                        else if (x <= 5 && y <= 5)
-                        {
-                            g = 4;
-                            temp3 = cells[g];
-                        }
-                        else if (x <= 5 && y<=8)
-                        {
-                            g = 5;
-                            temp3 = cells[g];
-                        }
-                        else if (x <= 8 && y <= 2)
-                        {
-                            g = 6;
-                            temp3 = cells[g];
-                        }
-                        else if (x <= 8 && y <= 5)
-                        {
-                            g = 7;
-                            temp3 = cells[g];
-                        }
-                        else if (x <= 8 && y <= 8)
-                        {
+                        g = (x / 3) + (y / 3) * 3;
+                        temp3 = cells[g];
 
-                            g = 8;
-                            temp3 = cells[g];
-                        }
-                        for(int i = 0; i < 100000; i++)
-                        {
+                        //if (x <= 2 && y <= 2)
+                        //{
+                        //    g = 0;
+                        //    temp3 = cells[g];
+                        //}
+                        //else if (x <= 2 && y <= 5)
+                        //{
+                        //    g = 1;
+                        //    temp3 = cells[g];
+                        //}
+                        //else if (x <= 2 && y <= 8)
+                        //{
+                        //    g = 2;
+                        //    temp3 = cells[g];
+                        //}
+                        //else if (x <= 5 && y <= 2)
+                        //{
+                        //    g = 3;
+                        //    temp3 = cells[g];
+                        //}
+                        //else if (x <= 5 && y <= 5)
+                        //{
+                        //    g = 4;
+                        //    temp3 = cells[g];
+                        //}
+                        //else if (x <= 5 && y <= 8)
+                        //{
+                        //    g = 5;
+                        //    temp3 = cells[g];
+                        //}
+                        //else if (x <= 8 && y <= 2)
+                        //{
+                        //    g = 6;
+                        //    temp3 = cells[g];
+                        //}
+                        //else if (x <= 8 && y <= 5)
+                        //{
+                        //    g = 7;
+                        //    temp3 = cells[g];
+                        //}
+                        //else if (x <= 8 && y <= 8)
+                        //{
+
+                        //    g = 8;
+                        //    temp3 = cells[g];
+                        //}
+                        for (int d = 0; d < 100000; d++)
+                        {                           
                             
+
                                 int a = rnd.Next(0, temp1.Count);
                                 int a1 = temp1[a];
-                                Console.WriteLine(a1 + " cols");
+
                                 int b = rnd.Next(0, temp2.Count);
                                 int b1 = temp2[b];
-                                Console.WriteLine(b1 + " rows");
+
                                 int c = rnd.Next(0, temp3.Count);
                                 int c1 = temp3[c];
-                          
-                            Console.WriteLine(c1 + " cells");
-                            if (a1 == b1 && b1 == c1)
+
+                            if (x == 8 && y == 8)
                             {
+                                if (cells[8].Count != 0)
+                                {
+                                    ohno = true;
+                                    x = 0;
+                                    y = 0;
+                                    
+                                    cols = colsex;
+                                    rows = rowsex;
+                                    cells = cellsex;
+                                    cont = true;
+                                    break;
+                                }
+                            }
+                            else if(ohno)
+                            {
+                                break;
+                            }
+
+                            
+                            else if (a1 == b1 && b1 == c1)
+                            {
+                                Console.WriteLine(x + " " + y);
                                 board[x, y] = a1;
-                                cols[x].Remove(a1);
-                                rows[y].Remove(b1);
-                                cells[g].Remove(c1);
+                                cells[g].Remove(board[x,y]);
+                                cols[x].Remove(board[x, y]);
+                                rows[y].Remove(board[x, y]);
+                                
                                 Suck++;
                                 for(int h = 0; h < 9; h++)
                                 {
@@ -138,7 +173,8 @@ namespace Sodoku
                     }
              if(Suck == 81)
                     {
-                        cont = false;
+                        
+                        
                     }
                         }
             }
